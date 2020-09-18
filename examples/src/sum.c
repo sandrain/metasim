a6ffb94e3b43f1191c17919d906ad7655dd8b562
Hyogi Sim <sandrain@gmail.com>
2020-09-18 16:26:28 -0400

    sum example update

--- examples/src/sum.c
+++ examples/src/sum.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <getopt.h>
 #include <mpi.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -24,24 +25,77 @@ static int server_nranks;
 
 static metasim_t metasim;
 
+static struct option l_opts[] = {
+    { "help", 0, 0, 'h' },
+    { "rank", 1, 0, 'r' },
+    { "serial", 0, 0, 's' },
+    { 0, 0, 0, 0 },
+};
+
+static char *s_opts = "hr:s";
+
+static char *usage_str =
+"\n"
+"Usage: sum [options...]\n"
+"\n"
+"-h, --help         print this help message\n"
+"-r, --rank=<R>     execute sum operation only on rank <R>\n"
+"                   (default: from all ranks)\n"
+"-s, --serial       execute from target ranks one by one\n"
+"                   (default: running in parallel)\n"
+"\n";
+
+static void print_usage(int ec)
+{
+    fputs(usage_str, stderr);
+    exit(ec);
+}
+
+static void do_sum(int32_t seed, int32_t expected)
+{
+    int ret = 0;
+    int32_t sum = 0;
+
+    ret = metasim_invoke_sum(metasim, seed, &sum);
+
+    __debug("[%d] RPC SUM (seed=%d) => (ret=%d, sum=%d), expected sum=%d",
+            rank, seed, ret, sum, expected);
+}
+
 int main(int argc, char **argv)
 {
     int ret = 0;
+    int serial = 0;
     int sum_rank = -1;
+    int ch = 0;
+    int ix = 0;
     int32_t seed = -1;
-    int32_t sum = 0;
     int32_t expected = 0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nranks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (argc == 2) {
-        sum_rank = atoi(argv[1]);
-        if (sum_rank >= nranks) {
-            __debug("%d is not a valid rank, rank 0 will be used", sum_rank);
-            sum_rank = 0;
+    while ((ch = getopt_long(argc, argv, s_opts, l_opts, &ix)) >= 0) {
+        switch (ch) {
+        case 'r':
+            sum_rank = atoi(optarg);
+            break;
+
+        case 's':
+            serial = 1;
+            break;
+
+        case 'h':
+        default:
+            print_usage(0);
+            break;
         }
+    }
+
+    if (sum_rank >= nranks) {
+        __debug("%d is not a valid rank, rank 0 will be used", sum_rank);
+        sum_rank = 0;
     }
 
     if (sum_rank >= 0)
@@ -70,15 +124,21 @@ int main(int argc, char **argv)
     expected = (server_nranks * (server_nranks - 1)) / 2;
     expected += server_nranks * seed;
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    if (sum_rank < 0 && serial) {
+        for (int i = 0; i < nranks; i++) {
+            MPI_Barrier(MPI_COMM_WORLD);
 
-    if (sum_rank < 0 || sum_rank == rank) {
-        ret = metasim_invoke_sum(metasim, seed, &sum);
-        __debug("[%d] RPC SUM (seed=%d) => (ret=%d,sum=%d), expected sum=%d",
-                rank, seed, ret, sum, expected);
+            if (i == rank)
+                do_sum(seed, expected);
+        }
+    } else {
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        if (sum_rank < 0 || sum_rank == rank)
+            do_sum(seed, expected);
+
+        MPI_Barrier(MPI_COMM_WORLD);
     }
-
-    MPI_Barrier(MPI_COMM_WORLD);
 
 out:
     metasim_exit(metasim);
